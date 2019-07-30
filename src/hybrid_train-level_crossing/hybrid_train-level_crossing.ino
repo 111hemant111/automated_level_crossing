9fc71f5ab78ed992d7d5dacb4f2534c9762b9054
Hemant Kuruva <111hemant111@gmail.com>
2019-07-30 05:02:36 -0400

    reorganized

--- src/hybrid_train-level_crossing/hybrid_train-level_crossing.ino
+++ src/hybrid_train-level_crossing/hybrid_train-level_crossing.ino
@@ -1,11 +1,3 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com> 
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://arduino.cc/en/Tutorial/Sweep
-*/ 
 
 #include <Servo.h> 
 #include <LiquidCrystal.h>
@@ -86,5 +78,3 @@ move--;
   }
  delay(100); 
 } 
-
-
